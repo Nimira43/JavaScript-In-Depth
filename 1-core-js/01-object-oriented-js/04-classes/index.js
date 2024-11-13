8ca4b791d81c5ec5de0d6e6ea50bda8495563720
Nimira43 <n.rabone72@aol.com>
2024-11-13 03:19:15 +0000

    Using triangle class

--- 1-core-js/01-object-oriented-js/04-classes/index.js
+++ 1-core-js/01-object-oriented-js/04-classes/index.js
@@ -23,3 +23,17 @@ class Triangle {
     return Math.sqrt(this.a ** 2 + this.b ** 2)
   }
 }
+console.log('----------')
+const triangle1 = new Triangle()
+console.log(triangle1.a = 34)
+console.log(triangle1.b = 29)
+console.log(triangle1.getArea())
+console.log(triangle1.getHypotenuse().toFixed(3))
+console.log(triangle1)
+console.log('----------')
+const triangle2 = new Triangle()
+console.log(triangle2.a = 39)
+console.log(triangle2.b = 87)
+console.log(triangle2.getArea())
+console.log(triangle2.getHypotenuse().toFixed(3))
+console.log(triangle2)
