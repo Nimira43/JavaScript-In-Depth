e76636a16a8014c87fd2c9d83f431f9beaafbf1c
Nimira43 <n.rabone72@aol.com>
2024-11-26 22:53:49 +0000

    Small demos of  object methods

--- 4-prototype-chain/01-Introduction/01-prototypal-inheritance.js
+++ 4-prototype-chain/01-Introduction/01-prototypal-inheritance.js
@@ -1,2 +1,17 @@
 let person = "Bobby"
-console.log(person.)
+console.log(person.toUpperCase())
+
+let array = [34, 94, 39, 28, 62, 82, 43]
+array.sort()
+
+console.log(array)
+
+let array2 = array.map(num => num * 2)
+array2
+
+/**
+ * Prototype Inheritance - acces object properties from another object
+ * 
+ * How do JavaScript objects know what methods they have?
+ * 
+ */
