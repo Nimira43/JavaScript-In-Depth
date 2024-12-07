c901189f484fed99d8853dffea7ad5fa07d52382
Nimira43 <n.rabone72@aol.com>
2024-12-07 09:24:20 +0000

    Setup withdrawal  method and created examples

--- 1-core-js/01-object-oriented-js/05-classes-mini-project/index.js
+++ 1-core-js/01-object-oriented-js/05-classes-mini-project/index.js
@@ -14,6 +14,16 @@ class BankAccount {
       return 'Deposit failed!'
     }
   }
+  withdraw(amount) {
+    if (amount < this.balance) {
+      this.balance -= amount
+      console.log(`Your withdrawal: £${amount} New balance: £${this.balance}`)
+      return 'Withdrawal successful!'
+    } else {
+      console.log('Cannot withdraw more than balance')
+      return 'Withdrawal failed!'
+    }
+  }
 }
 
 let account1 = new BankAccount('Bob', '1234567890', 25)
@@ -23,3 +33,20 @@ console.log(account1)
 console.log(account1.deposit(-378.67))
 console.log(account1)
 console.log(account1.deposit(38.93))
+console.log(account1)
+console.log(account1.withdraw(52.79))
+console.log(account1)
+console.log(account1.withdraw(89.50))
+console.log(account1)
+console.log(account1.deposit(529.05))
+console.log(account1)
+console.log(account1.deposit(422.61))
+console.log(account1)
+console.log(account1.withdraw(507.57))
+console.log(account1)
+console.log(account1.deposit(949.28))
+console.log(account1)
+console.log(account1.withdraw(631.77))
+console.log(account1)
+console.log(account1.deposit(484.81))
+console.log(account1)
