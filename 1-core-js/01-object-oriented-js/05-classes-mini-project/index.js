c4a9326b45faa6f3bd4fe577ca62bc42f5c0a0f9
Nimira43 <n.rabone72@aol.com>
2024-12-07 08:45:29 +0000

    Setup BankAccount class and constructor

---
+++ 1-core-js/01-object-oriented-js/05-classes-mini-project/index.js
@@ -0,0 +1,8 @@
+class BankAccount {
+  constructor(accountHolder, accountNumber, balance = 0) {
+    this.accountHolder = accountHolder
+    this.accountNumber = accountNumber
+    this.balance = balance
+  }
+}
+
