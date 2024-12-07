aebb8b7205fe607da655ba86883472e3d16689ce
Nimira43 <n.rabone72@aol.com>
2024-12-07 08:58:45 +0000

    Created deposit method

--- 1-core-js/01-object-oriented-js/05-classes-mini-project/index.js
+++ 1-core-js/01-object-oriented-js/05-classes-mini-project/index.js
@@ -4,5 +4,17 @@ class BankAccount {
     this.accountNumber = accountNumber
     this.balance = balance
   }
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount
+      console.log(`Your deposit: £${amount} New balance: £${this.balance}`)
+    } else {
+      console.log('Cannot desposit a negative number')
+    }
+  }
 }
 
+let transaction1 = new BankAccount('Bob', '1234567890', 25)
+console.log(transaction1)
+console.log(transaction1.deposit(78.32))
+console.log(transaction1)
