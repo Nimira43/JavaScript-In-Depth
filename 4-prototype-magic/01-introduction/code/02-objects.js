9e8d8fefbb6707c287b6613fa9a8c80cebcbbada
Nimira43 <n.rabone72@aol.com>
2025-01-01 01:36:20 +0000

    Created an array of objects - world cities - to use in this section.

---
+++ 4-prototype-magic/01-introduction/code/02-objects.js
@@ -0,0 +1,74 @@
+const cities = [
+  {
+    name: 'London',
+    founded: '43 AD',
+    area: '1,572 km²',
+    population: '8,982,000',
+    touristSites: ['Big Ben', 'London Eye', 'Tower of London']
+  },
+  {
+    name: 'Paris',
+    founded: '3rd century BC',
+    area: '105.4 km²',
+    population: '2,148,000',
+    touristSites: ['Eiffel Tower', 'Louvre Museum', 'Notre-Dame Cathedral']
+  },
+  {
+    name: 'Tokyo',
+    founded: '1603',
+    area: '2,194 km²',
+    population: '37,393,000',
+    touristSites: ['Tokyo Tower', 'Senso-ji Temple', 'Shibuya Crossing']
+  },
+  {
+    name: 'New York',
+    founded: '1624',
+    area: '783.8 km²',
+    population: '8,336,000',
+    touristSites: ['Statue of Liberty', 'Central Park', 'Times Square']
+  },
+  {
+    name: 'Rome',
+    founded: '753 BC',
+    area: '1,285 km²',
+    population: '2,873,000',
+    touristSites: ['Colosseum', 'Vatican Museums', 'Pantheon']
+  },
+  {
+    name: 'Sydney',
+    founded: '1788',
+    area: '12,367.7 km²',
+    population: '5,312,000',
+    touristSites: ['Sydney Opera House', 'Harbour Bridge', 'Bondi Beach']
+  },
+  {
+    name: 'Berlin',
+    founded: '1237',
+    area: '891.8 km²',
+    population: '3,769,000',
+    touristSites: ['Brandenburg Gate', 'Berlin Wall', 'Museum Island']
+  },
+  {
+    name: 'Moscow',
+    founded: '1147',
+    area: '2,511 km²',
+    population: '12,506,000',
+    touristSites: ['Red Square', 'Kremlin', 'Saint Basil\'s Cathedral']
+  },
+  {
+    name: 'Rio de Janeiro',
+    founded: '1565',
+    area: '1,221 km²',
+    population: '6,748,000',
+    touristSites: ['Christ the Redeemer', 'Sugarloaf Mountain', 'Copacabana Beach']
+  },
+  {
+    name: 'Beijing',
+    founded: '1045 BC',
+    area: '16,410.5 km²',
+    population: '21,540,000',
+    touristSites: ['Great Wall', 'Forbidden City', 'Temple of Heaven']
+  }
+]
+
+console.log(cities)
