e964ae2e158fd2e05ef98091cf606a0af6ba9166
Nimira43 <n.rabone72@aol.com>
2025-01-17 18:43:30 +0000

    Testing out an undeclared variable

--- 01-variables-in-depth/01-Introduction/temporalDeadZone.js
+++ 01-variables-in-depth/01-Introduction/temporalDeadZone.js
@@ -1,14 +1,19 @@
 // LET - Temporal Dead Zone
 // console.log(message1)
-let message1 = 'Will this work?'      // No      TDZ ends
-console.log(message1)                 // Yes 
+let message1 = 'Will this work?'         // No      TDZ ends
+console.log(message1)                    // Yes 
 
 // CONST - Temporal Dead Zone
 // console.log(message2)
-const message2 = 'Or this?'           // No      TDZ ends
-console.log(message2)                 // Yes
+const message2 = 'Or this?'              // No      TDZ ends
+console.log(message2)                    // Yes
 
 // VAR
 console.log(message3)
-var message3 = 'What about this?'     // Undefined
-console.log(message3)                 // Yes 
+var message3 = 'What about this?'        // Undefined
+console.log(message3)                    // Yes 
+
+// UNDECLARED VARIABLE
+// console.log(message4)                 // not defined
+message4 = 'Whats going to happen here?'
+console.log(message4)                    // It works (??)
