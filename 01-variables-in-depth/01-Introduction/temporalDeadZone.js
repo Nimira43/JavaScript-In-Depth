82c5f3f2014056c8d2f0bbd23b79616e3206d935
Nimira43 <n.rabone72@aol.com>
2025-01-17 18:36:40 +0000

    Reorganised code to show where TDZ starts and ends for let and const

--- 01-variables-in-depth/01-Introduction/temporalDeadZone.js
+++ 01-variables-in-depth/01-Introduction/temporalDeadZone.js
@@ -1,11 +1,14 @@
+// LET - Temporal Dead Zone
 // console.log(message1)
-// console.log(message2)
-console.log(message3)
-                                              
-let message1 = 'Will this work?'     // No
-const message2 = 'Or this?'          // No
-var message3 = 'What about this?'    // Undefined
+let message1 = 'Will this work?'      // No      TDZ ends
+console.log(message1)                 // Yes 
 
-console.log(message1)      // Yes
-console.log(message2)      // Yes
-console.log(message3)      // Yes 
+// CONST - Temporal Dead Zone
+// console.log(message2)
+const message2 = 'Or this?'           // No      TDZ ends
+console.log(message2)                 // Yes
+
+// VAR
+console.log(message3)
+var message3 = 'What about this?'     // Undefined
+console.log(message3)                 // Yes 
