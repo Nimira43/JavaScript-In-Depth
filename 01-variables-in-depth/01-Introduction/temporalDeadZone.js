ec024003c0d3656b44576c2a7ac050a00f078fa4
Nimira43 <n.rabone72@aol.com>
2025-01-17 18:23:54 +0000

    Simple example

---
+++ 01-variables-in-depth/01-Introduction/temporalDeadZone.js
@@ -0,0 +1,11 @@
+// console.log(message1)
+// console.log(message2)
+console.log(message3)
+                                              
+let message1 = 'Will this work?'     // No
+const message2 = 'Or this?'          // No
+var message3 = 'What about this?'    // Undefined
+
+console.log(message1)      // Yes
+console.log(message2)      // Yes
+console.log(message3)      // Yes 
