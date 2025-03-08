61fcab62f52fbe4cb04f14d3db84b374a5ead78b
Nimira43 <n.rabone72@aol.com>
2025-03-08 10:27:04 +0000

    Assigning a value directly from another variable.

--- 01-variables-in-depth/01-Introduction/Part-2/varIssues.js
+++ 01-variables-in-depth/01-Introduction/Part-2/varIssues.js
@@ -4,3 +4,13 @@ var message = 'This is a message'
 message = 'This is an updated message'
 // reassigning a new value to an existing variable. The memory address of the variable will bind and point to the new value. When garbage collection happens the original value will be removed.
 
+var x = 1 // memory address allocated to x and bound to the value of 1. 
+var y = x // new memory address allocated to y and it will be bound to the value 1.
+
+console.log('Value of x:', x)
+console.log('Value of y:', y)
+
+x = 4 // x given new value and it bound to 4
+
+console.log('Value of x:', x) // will be 4 
+console.log('Value of y:', y) // remains at 1
