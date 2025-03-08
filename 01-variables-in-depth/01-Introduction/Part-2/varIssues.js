3aae86620ce4d7a632f2dba7818f4979f0f3ad6a
Nimira43 <n.rabone72@aol.com>
2025-03-08 10:11:33 +0000

    Reassignment of variables.

---
+++ 01-variables-in-depth/01-Introduction/Part-2/varIssues.js
@@ -0,0 +1,6 @@
+var message = 'This is a message'
+// create a memory address for message variable. This memory address has created a binding pointing to the value of the variable.
+
+message = 'This is an updated message'
+// reassigning a new value to an existing variable. The memory address of the variable will bind and point to the new value. When garbage collection happens the original value will be removed.
+
