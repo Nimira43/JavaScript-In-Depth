578f6353759505a781ee2520e0fc9636420a9752
Nimira43 <n.rabone72@aol.com>
2025-04-02 20:08:05 +0100

    Cannot reassign const

---
+++ 01-variables-in-depth/01-Introduction/Part-2/const.js
@@ -0,0 +1,11 @@
+const message = "First message"
+console.log(message)
+
+message = "Second Message"
+
+/**
+ * 
+ *  const.js:4 
+    Uncaught TypeError: Assignment to constant variable.at const.js:4:9
+ * 
+ */
