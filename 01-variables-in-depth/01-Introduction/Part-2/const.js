a7c255a2f9828a9dfca2cee4f2d94e83221c6a48
Nimira43 <n.rabone72@aol.com>
2025-04-02 20:10:54 +0100

    Notes

--- 01-variables-in-depth/01-Introduction/Part-2/const.js
+++ 01-variables-in-depth/01-Introduction/Part-2/const.js
@@ -7,5 +7,10 @@ message = "Second Message"
  * 
  *  const.js:4 
     Uncaught TypeError: Assignment to constant variable.at const.js:4:9
+
+    Using const means you don't ever want to change
+    the binding map
  * 
  */
+
+/* However... */
