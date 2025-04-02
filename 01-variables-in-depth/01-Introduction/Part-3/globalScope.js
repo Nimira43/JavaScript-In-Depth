6559e2416361b1c4192adda5911eaedd1d367b85
Nimira43 <n.rabone72@aol.com>
2025-04-02 20:54:25 +0100

    Created globalScope.js and create three new variable. Console logging these.

---
+++ 01-variables-in-depth/01-Introduction/Part-3/globalScope.js
@@ -0,0 +1,7 @@
+var globalVar = 'Global VAR variable'
+var globalLet = 'Global LET variable'
+var globalConst = 'Global CONST variable'
+
+console.log(globalVar)
+console.log(globalLet)
+console.log(globalConst)
