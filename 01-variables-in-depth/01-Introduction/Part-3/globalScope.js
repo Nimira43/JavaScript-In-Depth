57a9081a004357d966818cfe0e4530567eee4233
Nimira43 <n.rabone72@aol.com>
2025-04-22 16:18:14 +0100

    Scope - 2

--- 01-variables-in-depth/01-Introduction/Part-3/globalScope.js
+++ 01-variables-in-depth/01-Introduction/Part-3/globalScope.js
@@ -2,14 +2,45 @@ var globalVar = 'Global VAR variable'
 let globalLet = 'Global LET variable'
 const globalConst = 'Global CONST variable'
 
-console.log('Outside Function - Var : ', globalVar)
-console.log('Outside Function - Let : ', globalLet)
-console.log('Outside Function - Const : ', globalConst)
+console.log('Outside Function - Global Var : ', globalVar)
+console.log('Outside Function - Global Let : ', globalLet)
+console.log('Outside Function - Global Const : ', globalConst)
 
 function showGlobal() {
-  console.log('Inside Function - Var : ', globalVar)
-  console.log('Inside Function - Let : ', globalLet)
-  console.log('Inside Function - Const : ', globalConst)
+  var localVar = 'Local VAR variable'
+  let localLet = 'Local LET variable'
+  const localConst = 'Local CONST variable'
+  console.log('Inside Function - Global Var : ', globalVar)
+  console.log('Inside Function - Global Let : ', globalLet)
+  console.log('Inside Function - Global Const : ', globalConst)
+  console.log('Inside Function - Local Var : ', localVar)
+  console.log('Inside Function - Local Let : ', localLet)
+  console.log('Inside Function - Local Const : ', localConst)
+
+
 }
 
 showGlobal()
+
+
+
+// console.log('Outside Function - Local Var : ', localVar)
+// console.log('Outside Function - Local Let : ', localLet)
+// console.log('Outside Function - Local Const : ', localConst)
+
+// Local variables to the showGlobal function cannot be accessed outside the function - including var
+
+/*
+Difference Between var, let, and const:
+
+var:
+
+Function-scoped.
+Can be redeclared in the same scope.
+
+let and const:
+
+Block-scoped (e.g., limited to {}).
+Cannot be accessed outside the block they are defined in.
+
+*/
