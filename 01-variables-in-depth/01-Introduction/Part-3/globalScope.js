d8280fca5d5a8a8cf4cd2b423ac0798fe9143dc5
Nimira43 <n.rabone72@aol.com>
2025-04-22 16:49:31 +0100

    Various examples of different scopes.

--- 01-variables-in-depth/01-Introduction/Part-3/globalScope.js
+++ 01-variables-in-depth/01-Introduction/Part-3/globalScope.js
@@ -1,32 +1,53 @@
+// Function Scope
+
 var globalVar = 'Global VAR variable'
 let globalLet = 'Global LET variable'
-const globalConst = 'Global CONST variable'
+const globalConst = 'Global CONST variable - I refuse to be reassigned!!!'
 
-console.log('Outside Function - Global Var : ', globalVar)
-console.log('Outside Function - Global Let : ', globalLet)
-console.log('Outside Function - Global Const : ', globalConst)
+console.log('Outside Function - Global VAR : ', globalVar)
+console.log('Outside Function - Global LET : ', globalLet)
+console.log('Outside Function - Global CONST : ', globalConst)
 
 function showGlobal() {
   var localVar = 'Local VAR variable'
   let localLet = 'Local LET variable'
-  const localConst = 'Local CONST variable'
-  console.log('Inside Function - Global Var : ', globalVar)
-  console.log('Inside Function - Global Let : ', globalLet)
-  console.log('Inside Function - Global Const : ', globalConst)
-  console.log('Inside Function - Local Var : ', localVar)
-  console.log('Inside Function - Local Let : ', localLet)
-  console.log('Inside Function - Local Const : ', localConst)
+  const localConst = 'Local CONST variable - I refuse to be reassigned!!!'
+  
+  console.log('Inside Function - Global VAR : ', globalVar)
+  console.log('Inside Function - Global LET : ', globalLet)
+  console.log('Inside Function - Global CONST : ', globalConst)
+  console.log('Inside Function - Local VAR : ', localVar)
+  console.log('Inside Function - Local LET : ', localLet)
+  console.log('Inside Function - Local CONST : ', localConst)
 
+  console.log(localVar = 'Alert!!! - localVar has changed inside function!!!')
+  console.log(localLet = 'Alert!!! - localLet has changed inside function!!!')
+   
+  globalVar = 'Alert!!! - globalVar has changed inside function!!!'
+  globalLet = 'Alert!!! - globalLet has changed inside function!!!'
 
+  // Cannot be reassigned
+  // globalConst = 'I want to change!!!'
+  // localConst = 'Me too!!!'
+
+  try {
+    globalConst = 'Attempting reassignment' // Throws TypeError
+  } catch (error) {
+    console.error('Error reassigning globalConst:', error.message)
+  }
+  
+  try {
+    localConst = 'Attempting reassignment' // Throws TypeError
+  } catch (error) {
+    console.error('Error reassigning localConst:', error.message)
+  }
 }
 
 showGlobal()
 
-
-
-// console.log('Outside Function - Local Var : ', localVar)
-// console.log('Outside Function - Local Let : ', localLet)
-// console.log('Outside Function - Local Const : ', localConst)
+// console.log('Outside Function - Local VAR : ', localVar)
+// console.log('Outside Function - Local LET : ', localLet)
+// console.log('Outside Function - Local CONST : ', localConst)
 
 // Local variables to the showGlobal function cannot be accessed outside the function - including var
 
@@ -44,3 +65,25 @@ Block-scoped (e.g., limited to {}).
 Cannot be accessed outside the block they are defined in.
 
 */
+
+console.log('OUTSIDE FUNCTION ALERT!!!! :-', globalVar)
+console.log('OUTSIDE FUNCTION ALERT!!!! :-', globalLet)
+
+// Block Scope
+
+if (true) {
+  var blockVar = 'I am block VAR'
+  let blockLet = 'I am block LET'
+  const blockConst = 'I am block CONST'
+
+  console.log('Inside if', blockVar) // Accessible here
+  console.log('Inside if', blockLet) // Accessible here
+  console.log('Inside if', blockConst) // Accessible here
+}
+
+// Accessible here (VAR leaks out of the block)
+console.log('Outside if', blockVar) 
+// ReferenceError
+// console.log('Outside if', blockLet)
+// ReferenceError
+// console.log('Outside if', blockConst) 
