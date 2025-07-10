8087c546a13da28868c105ea6fa73e1c7b9a033b
Nimira43 <n.rabone72@aol.com>
2025-07-10 03:27:13 +0100

    Implicit return

--- 07-general/03-javascript-for-react/arrowFunctions.js
+++ 07-general/03-javascript-for-react/arrowFunctions.js
@@ -1,11 +1,14 @@
 function getRectangleArea(width, height) {
   return width * height
 }
-
 console.log(getRectangleArea(7, 4))
 
+// Explicit return
 const getRectangleArea2 = (width, height) => {
   return width * height
 }
-
 console.log(getRectangleArea2(7, 4))
+
+// Implicit return
+const getRectangleArea3 = (width, height) =>  width * height
+console.log(getRectangleArea3(7, 4))
