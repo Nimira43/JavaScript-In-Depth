4183e4e69279d46c5b8c2b806baf3cd128f79911
Nimira43 <n.rabone72@aol.com>
2025-07-10 03:24:29 +0100

    Arrow function example

---
+++ 07-general/03-javascript-for-react/arrowFunctions.js
@@ -0,0 +1,11 @@
+function getRectangleArea(width, height) {
+  return width * height
+}
+
+console.log(getRectangleArea(7, 4))
+
+const getRectangleArea2 = (width, height) => {
+  return width * height
+}
+
+console.log(getRectangleArea2(7, 4))
