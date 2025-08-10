892ffcff70dba3efbec3b19cc6e6eb96ba12b5fd
Nimira43 <n.rabone72@aol.com>
2025-08-10 19:34:49 +0100

    Template literals - 2

--- 07-general/03-javascript-for-react/templateLiterals.js
+++ 07-general/03-javascript-for-react/templateLiterals.js
@@ -2,3 +2,10 @@ const name = 'Bob'
 const greeting = `Hello ${name}`
 
 console.log(greeting)
+
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp)
+
+  return date.toLocaleDateString() + ' at ' + date.toLocaleTimeString()
+}
+
